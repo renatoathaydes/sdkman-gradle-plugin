067999493f55b71f7c7f32c7c7f8f438f3b80aad
Lukasz Ochmanski <github@ochmanski.de>
2022-08-10 14:30:04 +0200

    java=11.0.2-open

--- examples/full-example/src/main/java/Foo.java
+++
@@ -1,6 +0,0 @@
-public class Foo {
-    public static void main( String[] args ) {
-        // use a JavaFX class just to prove this was compiled with the .sdkmanrc JDK (java=11.0.8.fx-zulu)
-        System.out.println( "Can see JavaFX? " + javafx.application.Application.class );
-    }
-}
